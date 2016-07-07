53fd32fba777d2bc0e5b03024fd1ad7d4d7fef81
golconda <flagrans@hotmail.com>
2016-07-07 14:58:02 +0200

    init commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.B1 = new System.Windows.Forms.Label();
+            this.B2 = new System.Windows.Forms.Label();
+            this.A2 = new System.Windows.Forms.Label();
+            this.B3 = new System.Windows.Forms.Label();
+            this.C1 = new System.Windows.Forms.Label();
+            this.C2 = new System.Windows.Forms.Label();
+            this.C3 = new System.Windows.Forms.Label();
+            this.A1 = new System.Windows.Forms.Label();
+            this.A3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(247, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.startToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // B1
+            // 
+            this.B1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.B1.AutoSize = true;
+            this.B1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.B1.Font = new System.Drawing.Font("Segoe UI Historic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B1.Location = new System.Drawing.Point(30, 177);
+            this.B1.MinimumSize = new System.Drawing.Size(40, 40);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(40, 40);
+            this.B1.TabIndex = 1;
+            this.B1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.B1.Click += new System.EventHandler(this.Lable_Click);
+            // 
+            // B2
+            // 
+            this.B2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.B2.AutoSize = true;
+            this.B2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.B2.Font = new System.Drawing.Font("Segoe UI Historic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B2.Location = new System.Drawing.Point(104, 177);
+            this.B2.MinimumSize = new System.Drawing.Size(40, 40);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(40, 40);
+            this.B2.TabIndex = 2;
+            this.B2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.B2.Click += new System.EventHandler(this.Lable_Click);
+            // 
+            // A2
+            // 
+            this.A2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.A2.AutoSize = true;
+            this.A2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.A2.Font = new System.Drawing.Font("Segoe UI Historic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A2.Location = new System.Drawing.Point(104, 114);
+            this.A2.MinimumSize = new System.Drawing.Size(40, 40);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(40, 40);
+            this.A2.TabIndex = 3;
+            this.A2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.A2.Click += new System.EventHandler(this.Lable_Click);
+            // 
+            // B3
+            // 
+            this.B3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.B3.AutoSize = true;
+            this.B3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.B3.Font = new System.Drawing.Font("Segoe UI Historic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B3.Location = new System.Drawing.Point(174, 177);
+            this.B3.MinimumSize = new System.Drawing.Size(40, 40);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(40, 40);
+            this.B3.TabIndex = 4;
+            this.B3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.B3.Click += new System.EventHandler(this.Lable_Click);
+            // 
+            // C1
+            // 
+            this.C1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.C1.AutoSize = true;
+            this.C1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C1.Font = new System.Drawing.Font("Segoe UI Historic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C1.Location = new System.Drawing.Point(30, 241);
+            this.C1.MinimumSize = new System.Drawing.Size(40, 40);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(40, 40);
+            this.C1.TabIndex = 5;
+            this.C1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.C1.Click += new System.EventHandler(this.Lable_Click);
+            // 
+            // C2
+            // 
+            this.C2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.C2.AutoSize = true;
+            this.C2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C2.Font = new System.Drawing.Font("Segoe UI Historic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C2.Location = new System.Drawing.Point(104, 241);
+            this.C2.MinimumSize = new System.Drawing.Size(40, 40);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(40, 40);
+            this.C2.TabIndex = 6;
+            this.C2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.C2.Click += new System.EventHandler(this.Lable_Click);
+            // 
+            // C3
+            // 
+            this.C3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.C3.AutoSize = true;
+            this.C3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.C3.Font = new System.Drawing.Font("Segoe UI Historic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C3.Location = new System.Drawing.Point(174, 241);
+            this.C3.MinimumSize = new System.Drawing.Size(40, 40);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(40, 40);
+            this.C3.TabIndex = 7;
+            this.C3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.C3.Click += new System.EventHandler(this.Lable_Click);
+            // 
+            // A1
+            // 
+            this.A1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.A1.AutoSize = true;
+            this.A1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.A1.Font = new System.Drawing.Font("Segoe UI Historic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A1.Location = new System.Drawing.Point(30, 114);
+            this.A1.MinimumSize = new System.Drawing.Size(40, 40);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(40, 40);
+            this.A1.TabIndex = 8;
+            this.A1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.A1.Click += new System.EventHandler(this.Lable_Click);
+            // 
+            // A3
+            // 
+            this.A3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.A3.AutoSize = true;
+            this.A3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.A3.Font = new System.Drawing.Font("Segoe UI Historic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A3.Location = new System.Drawing.Point(174, 114);
+            this.A3.MinimumSize = new System.Drawing.Size(40, 40);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(40, 40);
+            this.A3.TabIndex = 9;
+            this.A3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.A3.Click += new System.EventHandler(this.Lable_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Vems tur är det? ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(180, 48);
+            this.label2.MinimumSize = new System.Drawing.Size(40, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 40);
+            this.label2.TabIndex = 11;
+            this.label2.MouseLeave += new System.EventHandler(this.label2_MouseLeave);
+            this.label2.MouseHover += new System.EventHandler(this.label2_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 315);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Tic Tac Toe";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label B1;
+        private System.Windows.Forms.Label B2;
+        private System.Windows.Forms.Label A2;
+        private System.Windows.Forms.Label B3;
+        private System.Windows.Forms.Label C1;
+        private System.Windows.Forms.Label C2;
+        private System.Windows.Forms.Label C3;
+        private System.Windows.Forms.Label A1;
+        private System.Windows.Forms.Label A3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+       private int turn = 0;
+       private char c = 'X';
+      private  char[][] modelBoard;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+         
+        }
+
+     
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ett Tic Tac Toe i all sin enkelhet för att friska upp mina, Ulrika Golocondas, kunskaper i C#");
+        }
+
+        private void Lable_Click(object sender, EventArgs e)
+        {
+            ((Label)sender).Text = "" + c;
+            ((Label)sender).Enabled = false;
+           if(WinCheck())
+            {
+                MessageBox.Show(c + " har vunnit!");
+            }
+            turn++;
+            if (turn >= 9)
+            {
+                MessageBox.Show("Ingen har vunnit!");
+            }
+            else
+            {
+                if (turn % 2 == 0)
+                {
+                    c = 'X';
+                }
+                else
+                {
+                    c = '0';
+                }
+            }
+
+        }
+
+        private void label2_MouseHover(object sender, EventArgs e)
+        {
+            ((Label)sender).Text = "" + c;
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            ((Label)sender).Text = "";
+
+        }
+
+        private bool WinCheck() {
+            bool returnBool = false;
+         
+           
+
+            //Diagonalcheck
+            if ((A1.Text == B2.Text && A1.Text == C3.Text && A1.Text.Length > 0) || (A3.Text == B2.Text && A3.Text == C1.Text && A3.Text.Length > 0))
+            {
+                returnBool = true;
+            }
+
+
+            //Vågchekc
+            if ((A1.Text == A2.Text && A1.Text == A3.Text && A1.Text.Length >0) 
+                || (B1.Text == B2.Text && B1.Text == B3.Text && B1.Text.Length > 0)
+                || (C1.Text == C2.Text && C1.Text == C3.Text) && C1.Text.Length > 0)
+            {
+                returnBool = true;
+            }
+
+
+            //Höjdcheck
+            if ((A1.Text == B1.Text && A1.Text == B1.Text && A1.Text == C1.Text && A1.Text.Length > 0)
+                || (A2.Text == B2.Text && B2.Text == C2.Text && A2.Text== C2.Text && A2.Text.Length > 0)
+                || (A3.Text == B3.Text && A3.Text == B3.Text && A3.Text == C3.Text && A3.Text.Length > 0))
+            {
+                returnBool = true;
+            }
+
+            return returnBool; 
+        }
+    }
+}
